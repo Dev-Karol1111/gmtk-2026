138fce84a624b2c7b0708479f8fd23718e926e02
Dev Karol <karol.fojcik2@gmail.com>
2026-07-23 16:49:36 +0200

    Add hallway

--- scripts/autoload/management.gd
+++ scripts/autoload/management.gd
@@ -11,16 +11,15 @@ var ended_dialogs : Dictionary[String, bool] = {
 }
 
 func start_game() -> void:
-	switch_scene("res://scenes/hospital.tscn")
+	switch_scene("res://scenes/rooms/hospital.tscn")
+	await get_tree().create_timer(0.5).timeout
+	Signals.start_dialog.emit(load("res://assets/dialogs/wake_up.tres"))
 	mode = "dialog"
 	current_building = "hospital"
 
 func switch_scene(path: String) -> void:
 	get_tree().change_scene_to_file(path)
-	Signals.start_dialog.emit(load("res://assets/dialogs/wake_up.tres"))
-	await get_tree().create_timer(0.5).timeout
-	Signals.start_dialog.emit(load("res://assets/dialogs/wake_up.tres"))
-
+	
 func _process(delta: float) -> void:
 	check_dialogs()
 	
---
+++ scripts/rooms/hallway.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Management.current_building = "hallway"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	Management.switch_scene("res://scenes/rooms/hospital.tscn")
