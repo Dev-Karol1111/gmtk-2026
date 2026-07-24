2d3ce02b4f095dd7d991d2cb097f80e8d13dd2a3
Dev Karol <karol.fojcik2@gmail.com>
2026-07-24 15:37:16 +0200

    Update conversation UI

--- scripts/dialog_data.gd
+++ scripts/dialog_data.gd
@@ -4,6 +4,8 @@ class_name DialogData
 @export var id : String = ""
 @export var starting_id : String = ""
 @export var nodes : Array[DialogNode] = []
+@export var first_person : Texture2D
+@export var second_person : Texture2D
 
 func get_node(searching_id : String) -> DialogNode:
 	for node in nodes:
--- scripts/dialog_node.gd
+++ scripts/dialog_node.gd
@@ -5,5 +5,6 @@ extends Resource
 @export_multiline var text : String = ""
 @export var signal_to_emit : String = ""
 @export var end : bool = false
+@export_enum("first-person", "second-person") var person : String
 
 @export var options : Dictionary[String, String] = {}
